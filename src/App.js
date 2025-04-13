a2b31c74056537bda0f751b4748a4a38ae85e478
Huan Pham <huanpmd@gmail.com>
2025-04-13 16:39:28 +0700

    Test: temporarily expose /add-patient route directly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from "./pages/LoginPage";
+import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
+import PatientForm from './pages/PatientForm';
+import LabForm from './pages/LabForm';
+import ReviewPanel from './pages/ReviewPanel';
+import CheckUpPage from './pages/CheckUpPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
@@ -10,6 +15,21 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
+        <Route path="/add-patient" element={<PatientForm />} />
+
+        <Route
+          path="/lab-result"
+          element={<ProtectedRoute allowedRoles={['admin', 'lab']} element={<LabForm />} />}
+        />
+        <Route
+          path="/review"
+          element={<ProtectedRoute allowedRoles={['admin', 'reviewer']} element={<ReviewPanel />} />}
+        />
+        <Route
+          path="/check-up"
+          element={<ProtectedRoute allowedRoles={['admin', 'physician']} element={<CheckUpPage />} />}
+        />
       </Routes>
     </Router>
   );
