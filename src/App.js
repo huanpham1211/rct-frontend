428ccaab1ac0fde41216593b509cb69f84ac28f9
Huan Pham <huanpmd@gmail.com>
2025-04-13 09:09:28 +0700

    Fix import path for LoginPage

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from "./pages/LoginPage";
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
